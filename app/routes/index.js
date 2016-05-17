b49ac563e762d30d1ef7251d66863c1e2a2a4fbe
Jon Small <jonssmall@gmail.com>
2016-05-17 00:31:19 -0400

    basic toggle of view based on logged in state

--- app/routes/index.js
+++ app/routes/index.js
@@ -33,7 +33,7 @@ module.exports = function (app, passport) {
 	app.route('/logout')
 		.get(function (req, res) {
 			req.logout();
-			res.redirect('/login');
+			res.redirect('back');
 		});
 
 	app.route('/profile')
